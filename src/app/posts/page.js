0710cba9115f38ee5e3690c8fc6d23763b6b45f2
julianatonu <julianatonu@gmail.com>
2024-02-07 20:00:02 +0600

    add new page

---
+++ src/app/posts/page.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import React from 'react';
+
+const PostsPage = async () => {
+    const res = await fetch("http://localhost:5000/posts", {
+        next: {
+            revalidate: 5,
+        },
+    });
+
+    const posts = await res.json();
+    console.log(posts)
+    return (
+        <div>
+            <h1 className='text-3xl font-bold'>Total Post:{posts.length} </h1>
+            {posts.map((post, inbox) =>
+                <div key={post.id}
+                    className="card mx-auto w-[50%] my-5 bg-slate-300 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">{post.title}</h2>
+                        <p>{post.description}</p>
+                        <p>{post.like_count}</p>
+                        <div className="card-actions justify-end">
+                            <Link href={`/posts/${post.id}`}> <button className="btn btn-primary">See More</button>
+                            </Link>
+                        </div>
+
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default PostsPage;
