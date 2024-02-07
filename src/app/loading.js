a16efdcef77eb19a1a8395b9db1d46e6abc0da18
julianatonu <julianatonu@gmail.com>
2024-02-07 16:56:35 +0600

    add loading

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <div className='mt-10 text-center'>
+            <h1 className='text-blue-500 text-3xl font-bold'>Loading.......</h1>
+        </div>
+    );
+};
+
+export default LoadingPage;
