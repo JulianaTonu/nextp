b7b249d770f74c715db4c6955d8fb23f7fdeb21e
julianatonu <julianatonu@gmail.com>
2024-02-07 11:16:30 +0600

    add notfoundpage

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const NotFoundPage = () => {
+    return (
+        <div className='text-center mt-10'>
+            <h1 className='text-3xl text-red-500 font-bold'>404! Page Not Found.Sorry 😢</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
