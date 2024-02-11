8447f3ddd09b71e2f3dc717d0e90e98b930a2f4e
julianatonu <julianatonu@gmail.com>
2024-02-11 13:40:25 +0600

    post

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -3,3 +3,18 @@ import { NextResponse } from "next/server"
 export const GET = () => {
     return NextResponse.json({message:'Hello Tonu'})
 }
+
+//post api
+export const POST =()=>{
+    
+}
+
+//patch api
+export const PATCH =()=>{
+
+}
+
+//delete api
+export const DELETE =()=>{
+
+}
