ae7f40f41eab487958d06369e88acb46c35efb17
julianatonu <julianatonu@gmail.com>
2024-02-11 12:39:04 +0600

    add users

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,9 +1,35 @@
-import React from 'react';
 
-const DetailsPage = () => {
+
+export async function generateStaticParams(){
+    const res = await fetch("http://localhost:5000/posts");
+    const posts = await res.json();
+
+    const ids = posts.map((post) => {
+        return {
+            id: post.id + "",
+        }
+    })
+    console.log(ids)
+    return [{id:"1"}, {id:"2"}];
+}
+
+const DetailsPage = async ({ params }) => {
+
+    // const res = await fetch(`http://localhost:5000/posts/{params.id}`)
+    // const post = await res.json();
+    // console.log(params.id)
     return (
         <div>
             <h1>Post Details Page</h1>
+
+            {/* <div key={post.id}
+                    className="card mx-auto w-[50%] my-5 bg-slate-300 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">{post.title}</h2>
+                        <p>{post.description}</p>
+                        <p>{post.like_count}</p>
+                    </div>
+                </div> */}
         </div>
     );
 };
---
+++ src/component/Users/Users.js
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
+const Users = () => {
+    const [users, setUsers] =useState([]);
+
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res=>res.json())
+        .then(data=> console.log(data));
+    },[])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Users;
